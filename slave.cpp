1be38211b788f2602311ff833c7db54d5e11d651
ducky3333 <gnsco6157@gmail.com>
2023-07-05 11:14:59 +0900

    clean push

---
+++ slave.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+// int slave(int master_money, int master_profit)
+// {
+//     master_money += 10000;
+//     return master_money;
+// }
+
+// int main(){
+//     int master_money = 100000;
+//     printf("2023.07.05's property is %d", slave(master_money));
+//     return 0;
+// }
+
+int sum(int n)
+{
+    for(int n = 0; i < n; i++)
+    {
+        sum += i;
+    }
+}
+int main()
+{
+    int n;
+    std::cout<< This is Free Summrizer. Please enter any number << std::endl;
+    std::ctn >> n;
+    sum(n);
+    std::cout << The Result is << n << std::endl;
+    return 0;
+}
+    
